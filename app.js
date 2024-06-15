1db2a17825000b98ecfdb6822bfe242bb7b9229d
Thanalakshan <bredmendon@gmail.com>
2024-06-15 20:29:16 +0530

    modified:   app.js

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 function onClickedEstimatePrice() {
     console.log("Estimate price button clicked");
-    var sqft = document.getElementById("uiSqft");
-    var bhk = document.getElementById("uiBHK");
-    var bathrooms = document.getElementById("uiBathrooms");
+    var sqft = document.getElementById("uiSqft").value;
+    var bhk = document.getElementById("uiBHK").value;
+    var bathrooms = document.getElementById("uiBathrooms").value;
     var location = document.getElementById("uiLocations").value;
     var estPrice = document.getElementById("uiEstimatedPrice");
 
     var url = "https://thanalakshan.github.io/api/predict_home_price";
 
+    // Make sure to parse numerical inputs as floats or integers
     $.post(url, {
-        total_sqft: parseFloat(sqft.value),
-        bhk: bhk,
-        bath: bathrooms,
+        total_sqft: parseFloat(sqft),
+        bhk: parseInt(bhk),
+        bath: parseInt(bathrooms),
         location: location
     }, function(data, status) {
         console.log(data.estimated_price);
         estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
         console.log(status);
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Error in POST request:", textStatus, errorThrown);
     });
 }
+
 
 function onPageLoad() {
     var url = "./src/locations.json";
